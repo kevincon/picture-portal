69985c6ab364f61054f69ff3dc0a5efcd23d484b
Kevin Conley <kevindconley@gmail.com>
2012-03-18 22:41:36 -0700

    arduino code ready for testing for backend demo

---
+++ arduino_picture_portal/arduino_picture_portal.ino
@@ -0,0 +1,55 @@
+#include "SoftEasyTransfer.h"
+#include <SoftwareSerial.h>
+
+#define BUTTON_PIN                  3
+
+#define SERIAL_RX_PIN               8
+#define SERIAL_TX_PIN               9
+#define SERIAL_REQUEST_IMAGE_BYTE   0x1B
+
+#define IMAGE_WIDTH                 128
+#define IMAGE_HEIGHT                128
+#define IMAGE_MAX_FILENAME_LENGTH   50 
+
+SoftwareSerial serialComm(SERIAL_RX_PIN, SERIAL_TX_PIN);
+
+SoftEasyTransfer ET; 
+
+struct pp_image{
+  //int frame[IMAGE_WIDTH][IMAGE_HEIGHT];
+  char filename[IMAGE_MAX_FILENAME_LENGTH]; 
+};
+
+//this global var will hold data about the current image
+pp_image image_data;
+
+void setup() {
+  serialComm.begin(9600);
+  Serial.begin(9600);
+  //start the library, pass in the data details and the name of the serial port.
+  ET.begin(details(image_data), &serialComm);
+  
+  //setup lcd shield button for input, pull-up resistor, with interrupt on falling edge
+  pinMode(BUTTON_PIN, INPUT);
+  digitalWrite(BUTTON_PIN, HIGH);
+  attachInterrupt(1, button_press, FALLING);
+}
+
+/* 
+ * Callback for button press. Button is active-low.
+ */
+void button_press() {
+  //poor-man's debouncing, might need to change to an empty while loop
+  if(!digitalRead(BUTTON_PIN)) {
+    Serial.println("Requesting new image from server...");
+    serialComm.write(SERIAL_REQUEST_IMAGE_BYTE); 
+  }
+}
+
+void loop() {
+  if(ET.receiveData()){ //if we've received an image
+    Serial.print("Received image: ");
+    Serial.println(image_data.filename);
+  }
+  delay(2500);
+}
